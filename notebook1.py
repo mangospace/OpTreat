5cc9b3ae36c99f199d2be1f54482115e801e9f01
Manas Kaushik, MD ScD <kaushik.manas@gmail.com>
2023-11-25 16:11:46 -0500

    adding state centering

--- notebook1.py
+++ notebook1.py
@@ -18,27 +18,25 @@ st.caption("Oct 2023")
 st.caption('Made by @manas8u')
 st.caption('Please share your feedback and suggestions. DM @manas8u')
 
-"""
 option1 = st.selectbox(
     'Which state would you like to explore in detail?',
     (statelist))
 
-m = folium.Map(location=[48, -102], zoom_start=3)
+m1 = folium.Map(location=[48, -102], zoom_start=3)
 for x in range(len(full_data)):
     folium.Marker(
         location=[full_data.loc[x,'lat'],full_data.loc[x,'lon']],
         tooltip=full_data.loc[x,'CITY'],
         popup=full_data.loc[x,'PROVIDER NAME'],
         icon=folium.Icon(color="green"),
-    ).add_to(m)
-m
-#st_data = st_folium(m, width=725)
-"""
-option1="AL"
-state_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/state_center.csv')
+    ).add_to(m1)
+m1
+st_data = st_folium(m1, width=725)
+
+state_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/state_center.csv', dtype={'lat':np.int32,	'lon':np.int32})
 state_data=state_data[state_data["STATE"]==option1]
 state_data=state_data.reset_index(drop=True)
-state_data
+#state_data
 
 long=state_data.loc[0,'lon']
 latt=state_data.loc[0,'lat']
@@ -46,8 +44,7 @@ latt=state_data.loc[0,'lat']
 full_data1=full_data[full_data['STATE']==option1]
 full_data1=full_data1.reset_index(drop=True)
 full_data1
-#m = folium.Map(location=[long, latt], zoom_start=4)
-m = folium.Map(location=[32, -86], zoom_start=6)
+m = folium.Map(location=[latt, long], zoom_start=6)
 for x in range(len(full_data1)):
     folium.Marker(
         location=[full_data1.loc[x,'lat'],full_data1.loc[x,'lon']],
@@ -56,4 +53,4 @@ for x in range(len(full_data1)):
         icon=folium.Icon(color="green"),
     ).add_to(m)
 m
-#st_data = st_folium(m, width=725)
+st_data = st_folium(m, width=725)
