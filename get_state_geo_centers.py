8bb45caa9d80ee80a4a1a79d7ffb22ff5979d7d8
Manas Kaushik, MD ScD <kaushik.manas@gmail.com>
2023-11-25 15:52:21 -0500

    adding state centering

---
+++ get_state_geo_centers.py
@@ -0,0 +1,56 @@
+import requests
+import pandas as pd
+import regex as re
+
+url = 'https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States'
+html = requests.get(url).content
+df_list = pd.read_html(html)
+df = df_list[1]
+df
+
+daf=df['Coordinates'].str.split(pat="/",expand=True)
+daf['coord']=daf[1]
+daf=daf.drop([0,1], axis=1)
+daf=daf['coord'].str.split(pat=" ",expand=True)
+
+for x in range(len(daf)):
+    if re.findall('°N',daf.loc[x,1]):
+        daf.loc[x, 1]=re.sub('°N' ,'' ,daf.loc[x, 1])       
+        daf.loc[x, 'lat']=int(round(float(re.sub('\ufeff','',daf.loc[x, 1])),0))
+
+    if re.findall('°S',daf.loc[x,1]):
+        daf.loc[x, 1]=re.sub('°S' ,'' ,daf.loc[x, 1])       
+        daf.loc[x, 'lat']=round(float(re.sub('\ufeff','',daf.loc[x, 1])),0)*-1
+
+
+    if re.findall('°W',daf.loc[x,2]):
+        daf.loc[x, 2]=re.sub('°W' ,'' ,daf.loc[x, 2])       
+        daf.loc[x, 'lon']=int(round(float(re.sub('\ufeff','',daf.loc[x, 2])),0))
+
+    if re.findall('°E',daf.loc[x,2]):
+        daf.loc[x, 2]=re.sub('°E' ,'' ,daf.loc[x, 2])       
+        daf.loc[x, 'lon']=round(float(re.sub('\ufeff','',daf.loc[x, 2])),0)*-1
+
+
+daf=daf[['lat','lon']]
+
+daf=pd.merge(daf, df, left_index=True, right_index=True, how="outer")
+
+
+url = 'https://www.faa.gov/air_traffic/publications/atpubs/cnt_html/appendix_a.html'
+html = requests.get(url).content
+df_list = pd.read_html(html)
+df = df_list[0]
+df1=df[['STATE(TERRITORY)','STATE(TERRITORY).1']]
+df1=df1.rename(columns={'STATE(TERRITORY)':'State or federal district','STATE(TERRITORY).1':"STATE"})
+df2=df[['STATE(TERRITORY).2','STATE(TERRITORY).3']]
+df2=df2.rename(columns={'STATE(TERRITORY).2':'State or federal district','STATE(TERRITORY).3':"STATE"})
+df3=df[['STATE(TERRITORY).4','STATE(TERRITORY).5']]
+df3=df3.rename(columns={'STATE(TERRITORY).4':'State or federal district','STATE(TERRITORY).5':"STATE"})
+df4=pd.concat([df1,df2,df3])
+df4=df4.reset_index(drop=True)
+
+daf1=pd.merge(daf, df4, left_on='State or federal district', right_on="State or federal district", how="outer")
+daf1=daf1.drop(columns=['Location','Coordinates'])
+daf1=daf1.dropna(subset=['lat','lon'])
+daf1.to_csv(r"D:\Data\OpTreat\state_center.csv")
--- notebook1.py
+++ notebook1.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import requests
 import pandas as pd
-import json
-import regex as re
 import numpy as np
 import folium
 import streamlit as st
-from streamlit.web import cli as stcli
 from streamlit_folium import st_folium
 #full_data=pd.read_csv('/Users/manaskaushik/Documents/OpiodTreat/full_data.csv')
 
-full_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/full_data.csv')
-full_data=full_data.dropna()
-full_data=full_data.reset_index(drop=True)
-fd=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/suppl.csv')
-full_data=pd.concat([full_data, fd])
+full_data=pd.read_csv('https://raw.githubusercontent.com/mangospace/OpTreat/main/oct_data.csv')
+full_data.reset_index(drop=True)
+
 statelist=full_data['STATE'].tolist()
 
 st.title('Opiod Treatment Centers')
@@ -24,9 +18,10 @@ st.caption("Oct 2023")
 st.caption('Made by @manas8u')
 st.caption('Please share your feedback and suggestions. DM @manas8u')
 
-#option1 = st.selectbox(
-#    'Which state would you like to explore in detail?',
-#    (statelist))
+"""
+option1 = st.selectbox(
+    'Which state would you like to explore in detail?',
+    (statelist))
 
 m = folium.Map(location=[48, -102], zoom_start=3)
 for x in range(len(full_data)):
@@ -37,4 +32,20 @@ for x in range(len(full_data)):
         icon=folium.Icon(color="green"),
     ).add_to(m)
 m
+#st_data = st_folium(m, width=725)
+"""
+option1="AL"
+full_data1=full_data[full_data['STATE']==option1]
+full_data1=full_data1.reset_index(drop=True)
+full_data1
+m = folium.Map(location=[48, -102], zoom_start=3)
+for x in range(len(full_data1)):
+    folium.Marker(
+        location=[full_data1.loc[x,'lat'],full_data1.loc[x,'lon']],
+        tooltip=full_data1.loc[x,'CITY'],
+        popup=full_data1.loc[x,'PROVIDER NAME'],
+        icon=folium.Icon(color="green"),
+    ).add_to(m)
+m
 st_data = st_folium(m, width=725)
+""""""
